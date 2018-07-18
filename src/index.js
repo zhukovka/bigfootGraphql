25156083232fdc57e9d9a8b0205c753e314d31ab
zhukovka <helen.zhukova@gmail.com>
2018-07-18 21:38:22 +0300

    app with workers

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import BigfootDB from './model/BigfootDB';
 import {graphql, buildSchema} from 'graphql';
 import root from './model/resolvers';
 
-console.log("hello 42!");
 
+const projectId = 'charmed20';
+console.log(projectId);
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/serviceworker.js").then(function (registration) {
+        console.log("Service Worker registered with scope:", registration.scope);
+    }).catch(function (err) {
+        console.log("Service Worker registration failed:", err);
+    });
+}
+
+const worker = new Worker('worker.js');
+worker.onmessage = function (e) {
+    console.log('Message received from worker', event.data);
+
+    const {job, status} = event.data;
+    if (status === 'done') {
+        switch (job) {
+            case 'fetchProject':
+                main();
+                break;
+        }
+    }
+};
+worker.postMessage({job: 'fetchProject', payload: projectId});
 
 async function main () {
-    const project = await fetch('/api/project').then(r => r.json());
-    console.log(project);
-    const db = new BigfootDB(project.id, 1);
-    // const m = await db.populateMatches(project.matches);
+    const db = new BigfootDB(projectId, 1);
     const context = {db};
     // Construct a schema, using GraphQL schema language
     const schema = await fetch('/api/schema').then(r => r.text()).then(typeDefs => buildSchema(typeDefs));
@@ -21,7 +42,5 @@ async function main () {
     });
 
 }
-
-main();
 
 
